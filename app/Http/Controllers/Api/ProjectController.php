2608c4d54aa8931720269a1ea333e5dc1762fcf5
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-12 19:03:34 +0200

    feat: add Api ProjectController and return projects list in json

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // prendo dati dal database
+        $projects = Project::paginate(10);
+        
+        // ritorno i dati nel formato json
+        return response()->json($projects);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+}
