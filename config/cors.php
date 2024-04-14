ff3c76b06aa21f045c711f1f32c6a20985cb8745
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-14 15:46:54 +0200

    feat: add allowed origins patterns

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['http://localhost:5173','http://localhost:5174'],
 
     'allowed_headers' => ['*'],
 
